528f7491737c00d06b1e101f63c27fdf820c34df
CarlosCruz1503 <carlosjose445566@gmail.com>
2023-02-28 15:27:59 -0500

    with validations create quiz but whiout correct

--- Front/src/pages/createQuiz/createPage.tsx
+++ Front/src/pages/createQuiz/createPage.tsx
@@ -14,74 +14,194 @@ export default function IcreatePage({ }: Props) {
 
     const quizSchema: any = yup.object().shape({
         name: yup.string()
-            .required("Debes poner un nombre a tu quiz")
+            .required("Es necesario un nombre para tu quiz")
             .min(4, "El nombre del quiz debe de ser minimo 4 caracteres")
             .max(20, "El nombre del quiz debe de ser maximo de 20 caracteres"),
-        questions: yup.array()
-            .of(
-                yup.object().shape({
-                    questionsTitle: yup.string().min(4, 'too short').max(20, "demasiado texto").required('Required'), // these constraints take precedence // these constraints take precedence
-                })
-            )
-            .min(4, 'Minimum of 3 friends')
-            .required('Must have friends'), // these constraints are shown if and only if inner constraints are satisfied
+        questions: yup.array().of(
+            yup.object().shape({
+                questionsTitle: yup.string().required("La pregunta debe tener un titulo"),
+                answers: yup.array().of(
+                    yup.object().shape({
+                        answerTitle: yup.string().required("Todas las preguntas deben tener 4 respuestas")
+                            .min(4, "La respuesta debe ser de minimo 4 caracteres")
+                            .max(50, "La respuesta puede ser de maximo 50 caracteres"),
+                    }),
+                ),
+                correct: yup.number()
+            }),
+        ), // these constraints are shown if and only if inner constraints are satisfied
         private: yup.string().required(),
         author: yup.string()
-            .required("Escribe el nombre del autor del quiz")
+            .required("Es necesario el nombre del autor del quiz")
             .min(4, "El nombre del autor debe de ser minimo 4 caracteres")
             .max(20, "El nombre del autor debe de ser maximo de 20 caracteres")
     })
 
     const initialValues = {
         name: "",
-        private: "true",
-        author: ""
+        private: "false",
+        author: "",
+        questions: [{
+            questionsTitle: "",
+            answers: [
+                {
+                    answerTitle: "",
+                },
+                {
+                    answerTitle: "",
+                },
+                {
+                    answerTitle: "",
+                },
+                {
+                    answerTitle: "",
+                }
+            ],
+            correct: 1,
+        }, {
+            questionsTitle: "",
+            answers: [
+                {
+                    answerTitle: "",
+                },
+                {
+                    answerTitle: "",
+                },
+                {
+                    answerTitle: "",
+                },
+                {
+                    answerTitle: "",
+                }
+            ],
+            correct: 1,
+        },
+        {
+            questionsTitle: "",
+            answers: [
+                {
+                    answerTitle: "",
+                },
+                {
+                    answerTitle: "",
+                },
+                {
+                    answerTitle: "",
+                },
+                {
+                    answerTitle: "",
+                }
+            ],
+            correct: 1,
+        },
+        {
+            questionsTitle: "",
+            answers: [
+                {
+                    answerTitle: "",
+                },
+                {
+                    answerTitle: "",
+                },
+                {
+                    answerTitle: "",
+                },
+                {
+                    answerTitle: "",
+                }
+            ],
+            correct: 1,
+        }]
     }
 
     function arrayQuestionsFunction(values: any, errors: any, touched: any) {
-        let arrayQuestions = []
-        for (let i = 0; i < nQuestions; i++) {
-            arrayQuestions.push(
-                <FieldArray
-                    name="questions"
-                    render={({ form }) => (
-                        <div className='col-12 col-md-6'>
-                            <div className="list-group">
-                                <h3 className='text-center'>Titulo de la pregunta #{`${i + 1}`}</h3>
-                                <Field id={`questions.${i}.questionsTitle`} name={`questions.${i}.questionsTitle`} type="text" className="field-form p-3" placeholder={`Pregunta #${i + 1}`} />
-                                {
-                                    listAnswer(i)
-                                }
 
-                                {errors.questions &&
-                                    errors.questions[i] &&
-                                    errors.questions[i] &&
-                                    touched.questions[i] &&
-                                     (
-                                        <div className="field-error">
-                                            {errors.questions[i]}
+        return (
+            <FieldArray
+                name="questions"
+                render={arrayHelpers => {
+                    const questions = values.questions;
+                    return (
+                        <div className='row'>
+
+                            {questions && questions.length > 0
+                                ? questions.map((user: any, index: any) => (
+                                    <div key={index} className='col-12 col-md-6 text-center col-questions' >
+                                        <h1>Pregunta #{`${index + 1}`}</h1>
+                                        <ErrorMessage name={`questions.${index}.questionsTitle`} component="h4" className='text-danger' />
+                                        <Field
+                                            name={`questions.${index}.questionsTitle`}
+                                            className="field-form p-3"
+                                            placeholder={`Pregunta #${index + 1}`}
+                                        />
+                                        <br />
+
+
+
+                                        {listAnswer(index)}
+
+                                        <h5 className='text-center mt-5'>Cual es la respuesta correcta a la pregunta? </h5>
+                                        <div className="answers-yes mb-5">
+                                            <br />
+                                            <h2>#1</h2>
+                                            <Field type="radio" id={`questions.${index}.correct.${index}`} name={`questions.${index}.correct.${index}`} value="1" checked />
+                                            <h2>#2</h2>
+                                            <Field type="radio" id={`questions.${index}.correct.${index}`} name={`questions.${index}.correct.${index}`} value="2" />
+                                            <h2>#3</h2>
+                                            <Field type="radio" id={`questions.${index}.correct.${index}`} name={`questions.${index}.correct.${index}`} value="3" />
+                                            <h2>#4</h2>
+                                            <Field type="radio" id={`questions.${index}.correct.${index}`} name={`questions.${index}.correct.${index}`} value="4" />
                                         </div>
-                                    )}
-                                <h5 className='text-center'>Cual es la respuesta correcta a la pregunta? </h5>
-                                <div className="answers-yes">
-                                    <h2>#1</h2>
-                                    <Field type="radio" id={`questions.${i}.correct.${i + 1}`} name={`questions.${i}.correct.${i + 1}`} value="1" checked />
-                                    <h2>#2</h2>
-                                    <Field type="radio" id={`questions.${i}.correct.${i + 1}`} name={`questions.${i}.correct.${i + 1}`} value="2" />
-                                    <h2>#3</h2>
-                                    <Field type="radio" id={`questions.${i}.correct.${i + 1}`} name={`questions.${i}.correct.${i + 1}`} value="3" />
-                                    <h2>#4</h2>
-                                    <Field type="radio" id={`questions.${i}.correct.${i + 1}`} name={`questions.${i}.correct.${i + 1}`} value="4" />
-                                </div>
-                            </div>
-                        </div>
-                    )
-                    }
 
-                />
-            )
-        }
-        return (arrayQuestions)
+                                        {index >= 4
+                                            ?
+                                            <button
+                                                className='btn m-0 mt-4'
+                                                type="button"
+                                                onClick={() => arrayHelpers.remove(index)} // remove a friend from the list
+                                            > Eliminar esta pregunta </button>
+                                            :
+                                            <></>
+                                        }
+                                    </div>
+                                ))
+                                : null}
+                            <button
+                                className='btn m-0 mt-3'
+                                type="button"
+                                onClick={() =>
+                                    arrayHelpers.push({
+                                        questionsTitle: "",
+                                        answers: [
+                                            {
+                                                answerTitle: "",
+                                            },
+                                            {
+                                                answerTitle: "",
+                                            },
+                                            {
+                                                answerTitle: "",
+                                            },
+                                            {
+                                                answerTitle: "",
+                                            }
+                                        ]
+                                    })
+                                } // insert an empty string at a position
+                            >
+                                Añadir una preguntas más
+                            </button>
+                            <br />
+                            <br />
+                            <br />
+                        </div >
+
+                    );
+
+                }}
+
+            />
+        )
     }
 
     function listAnswer(i: number) {
@@ -92,8 +212,9 @@ export default function IcreatePage({ }: Props) {
         for (let j = 0; j < 4; j++) {
             arrayAnswers.push(
                 <div>
-                    <h4>Escribe la respuesta #{`${j + 1}`}</h4>
-                    <input type="text" name={`questions.${i}.AnswerTitle.${i + j}`} maxLength={20} minLength={4} />
+                    <h4 className='mt-3'>Escribe la respuesta #{`${j + 1}`}</h4>
+                    <Field type="text" name={`questions.${i}.answers.${j}.answerTitle`} className="form-control" />
+                    <ErrorMessage name={`questions.${i}.answers.${j}.answerTitle`} component="h5" className='text-danger' />
                 </div>
             )
         }
@@ -122,7 +243,7 @@ export default function IcreatePage({ }: Props) {
                                         {
                                             errors.name && touched.name && (
                                                 <div>
-                                                    <ErrorMessage component="h5" name="name"></ErrorMessage>
+                                                    <ErrorMessage component="h5" name="name" className='text-danger'></ErrorMessage>
                                                 </div>
                                             )
                                         }
@@ -148,49 +269,34 @@ export default function IcreatePage({ }: Props) {
                                             {preview ? <img className='img-profile img-fluid form-image' src={preview} alt="x" /> : <img className='img-profile img-fluid form-image' src={`https://i.pinimg.com/550x/b5/46/3c/b5463c3591ec63cf076ac48179e3b0db.jpg`} alt="x" />}
                                         </div>
 
-                                        <h3 className='text-center'>Quieres que este quiz sea privado y solo se pueda jugar compartiendo el codigo del quiz?</h3>
-                                        <input id="private1" name="private" type="radio" className="" value="true" />
-                                        {
-                                            errors.private && touched.private && (
-                                                <div>
-                                                    <ErrorMessage component="h5" name="private"></ErrorMessage>
-                                                </div>
-                                            )
-                                        }
+                                        <div className="form-check ">
+                                            <input id="private1" name="private" type="radio" className="" value="true" />
+                                            <label className="form-check-label m-2 mt-0 mb-0" htmlFor="private">
+                                                Este Quiz es Privado
+                                            </label>
+                                        </div>
 
-
-
+                                        <div className="form-check ">
+                                            <input id="private1" name="private" type="radio" className="" value="false" checked />
+                                            <label className="form-check-label m-2 mt-0 mb-0" htmlFor="private">
+                                                Este Quiz es Publico
+                                            </label>
+                                        </div>
 
                                         <h3 className='text-center'>Quien es el autor de este quiz</h3>
                                         <Field id="author" name="author" type="text" className="field-form" />
                                         {
                                             errors.author && touched.author && (
                                                 <div>
-                                                    <ErrorMessage component="h5" name="author"></ErrorMessage>
+                                                    <ErrorMessage component="h5" name="author" className='text-danger'></ErrorMessage>
                                                 </div>
                                             )
                                         }
-
-
-                                        <form action="" name="" onSubmit={(evt) => {
-                                            evt.preventDefault()
-                                            console.log(evt)
-                                        }}>
-                                            <div className="row">
-                                                {arrayQuestionsFunction(values, errors, touched)}
-                                            </div>
-                                        </form>
-
-
-
-
-                                        <button className='btn btn-dark' onClick={() => {
-                                            let numberQ = nQuestions + 1
-                                            setNQuestions(nQuestions => nQuestions + 1)
-                                        }}> Agregar una pregunta mas</button>
+                                        <h2>Agrega preguntas a tu Quiz</h2>
+                                        {arrayQuestionsFunction(values, errors, touched)}
 
                                         {isSubmitting ? (<p>Creando tú publicación</p>) : null}
-                                        <button type="submit" className='btn'>Crear Tu publiacion</button>
+                                        <button type="submit" className='btn'><h1>Crear Quiz</h1></button>
                                     </Form>)
                             }}
 
